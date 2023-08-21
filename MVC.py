de0c5c3b6270c2a84ad7367d5156f7bb39416735
Seth Banta <sethbanta@u.boisestate.edu>
2023-08-21 07:54:22 -0600

    Adjusted default positioning
    Updated the GUI to open at the center of the users screen by grabbing the current width and height of the users machine. Also set the default size to 1280x960

--- MVC.py
+++ MVC.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
 import sys
+import tkinter as tk
 
 #Create instance of QApplication
 app = QApplication([])
@@ -11,7 +12,14 @@ mainMenu = QWidget()
 window = QStackedLayout()
 #Set the title of our main menu widget
 mainMenu.setWindowTitle("Customer data Python MVC")
-
+#Grab resolution of monitor in order to try and place the window in the center of the screen
+root = tk.Tk()
+screen_width = root.winfo_screenwidth() / 2
+screen_height = root.winfo_screenheight() / 2
+#Use the grabbed width of the monitor and subtract half the width of the window size in order to offset
+#the starting area to be close enough to the center of the screen
+#same method is used for height as well
+mainMenu.setGeometry((int(screen_width) - 640),(int(screen_height) - 480),1280,960)
 
 #Add widget to window for display
 window.addWidget(mainMenu)
