86b3b0a684cde66057467d50a0a7652e36afb89f
Seth Banta <sethbanta@u.boisestate.edu>
2023-08-21 11:30:16 -0600

    Added test api button
    Added a button that is in the center of the window that is labelled "Test API connectivity" so that the first thing the user does is test that the API is online and running, once more features are added, it will make other controls visible once the program has ensured the API is running

--- MVC.py
+++ MVC.py
@@ -1,6 +1,17 @@
 from PyQt5.QtWidgets import *
 import sys
 import tkinter as tk
+import requests
+
+api_url = "https://localhost:7180/customer"
+
+def testButtonAction():
+    #Send a get request to the API to get all customers, if the API is running and responds appropriately
+    #we should receive a 200 response
+    response = requests.get("https://localhost:7180/customer/GetAllCustomers", verify=False)
+    #if an OK response is received, show the rest of the controls
+    if(response.status_code == 200):
+        print(f'API running, show buttons')
 
 #Create instance of QApplication
 app = QApplication([])
@@ -20,6 +31,15 @@ screen_height = root.winfo_screenheight() / 2
 #the starting area to be close enough to the center of the screen
 #same method is used for height as well
 mainMenu.setGeometry((int(screen_width) - 640),(int(screen_height) - 480),1280,960)
+
+#Create a new button for testing connectivity to the API
+#using this button to server as an initial test for users to run to make sure the API is running, once the test
+#is completed, other controls will display accordingly, this is to avoid sending requests to an offline API
+testButton = QPushButton(mainMenu)
+testButton.setText("Test API connectivity")
+#move the starting point to the center of the screen then subtract the width and height of the button
+testButton.move(int(1280/2) - testButton.size().width(), int(960/2) - testButton.size().height())
+testButton.clicked.connect(testButtonAction)
 
 #Add widget to window for display
 window.addWidget(mainMenu)
